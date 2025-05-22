7116cef2161600cb1591dd38756a038016732f8a
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-05-22 13:17:15 -0300

    Projects #1

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,27 +10,15 @@ const Projects = () => {
         [
             {
                 title: "NutriAção",
-                date: "22-02-2022",
+                date: "2025",
                 img: "/work1.png",
                 link: "https://nutri-view.vercel.app"
             },
             {
-                title: "work2",
-                date: "26-05-2020",
-                img: "/work1.jpg",
-                link: ""
-            },
-            {
-                title: "work3",
-                date: "26-05-2026",
-                img: "/work1.jpg",
-                link: ""
-            },
-            {
-                title: "work4",
-                date: "26-05-2026",
-                img: "/work1.jpg",
-                link: ""
+                title: "Portfolio",
+                date: "2025",
+                img: "/work2.png",
+                link: "https://ntanzi-portfolio.vercel.app"
             },
         ]
 
@@ -40,52 +28,41 @@ const Projects = () => {
         offset: ["start start", "end end"]
     })
 
+    const pageHeight = MyWorks.length * 150;
+    console.log(pageHeight)
 
     return (
-        <motion.section ref={targetRef} className={`relative h-[400vh]`}>
-            <div className={`top-0 sticky h-[100vh] padding-x py-5 flex flex-col overflow-hidden`}>
+        <motion.section ref={targetRef}
+            className={`relative `}>
+            <div className={`padding-x py-5 flex flex-col overflow-hidden`}>
                 <div>
                     <h2 className='text-[3em]'>Projects</h2>
                     <hr className='bg-white h-[2px] w-full mb-[2em]' />
                 </div>
-                <div className='relative w-full h-full'>
-                    {MyWorks.map((itens, i) => {
+                <div className='relative flex flex-col gap-5 w-full h-full'>
+                    {MyWorks.map((item, i) => {
 
-                        const walgDeg = (MyWorks.length - 1) * 10;
-                        const difGrau = (i * 10);
-
-                        const starDeg = 0 - difGrau;
-                        const centerDeg = (walgDeg / 2) - difGrau;
-                        const endDeg = walgDeg - difGrau;
-
-                        const rotateZ = useTransform(scrollYProgress, [0, .5, 1], [`${starDeg}deg`, `${centerDeg}deg`, `${endDeg}deg`]);
-
-                        const ref = useRef(null);
-
+                       
 
                         return (
                             <motion.div
-
                                 key={i}
-                                className={`absolute `}
+                                className={` md:w-[55vw] w-full bg-[var(--background)]`}
                                 style={{
-                                    rotateZ,
-                                    top: `50%`,
-                                    left: `50%`,
-                                    translateY: `-50%`,
-                                    translateX: `-50%`,
-                                    transformOrigin: `50% 400vh`,
 
                                 }}
                             >
                                 <motion.div
-                                    ref={ref}
                                     style={{
-                                    
+
                                     }}
-                                    className='relative  sm:w-[30em] sm:h-[20em] w-[85vw] h-[15em] right-0 hover:scale-[1.3] transition-all'>
-                                    <Image src={itens.img} alt={itens.img} fill className='object-cover' />
+                                    className='relative  right-0 transition-all'>
+                                    <img src={item.img} alt={item.img} className='object-cover' />
                                 </motion.div>
+                                <div className='w-full flex justify-between text-xl'>
+                                    <h2>{item.title}</h2>
+                                    <h2 className='tracking-widest'>{item.date}</h2>
+                                </div>
                             </motion.div>
                         )
                     })
