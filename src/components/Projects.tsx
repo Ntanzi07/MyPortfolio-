c65613da56a3ada6827597afda551d7bda80dedb
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-05-23 17:24:45 -0300

    fixing error in projects  and fixing mobile

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -62,7 +62,7 @@ const Projects = () => {
   const [height, setHeight] = useState(1);
 
   const itemsRef = useRef<HTMLDivElement>(null);
-  const [heightItems, setHeightItems] = useState(1);
+  const [heightTitle, setHeightTitle] = useState(1);
 
   const mouseY = useSpring(rawMouseY, {
     stiffness: 100,
@@ -81,7 +81,7 @@ const Projects = () => {
       rawMouseY.set(containerRef.current.offsetHeight / 2);
     }
     if (itemsRef.current) {
-      setHeightItems(itemsRef.current.offsetHeight);
+      setHeightTitle(itemsRef.current.offsetHeight);
     }
 
   }, []);
@@ -113,7 +113,7 @@ const Projects = () => {
     [0, height],
     isMobile
       ? [`0`, `0`]
-      : [`${heightItems}px`, `-${heightItems}px`]);
+      : [`50%`, `-50%`]);
 
   const scale = useTransform(
     scaleItem,
@@ -122,19 +122,23 @@ const Projects = () => {
   );
 
   return (
-    <motion.section className={`relative`}>
-      <div className={` md:h-screen padding-x py-5 flex flex-col`}>
-        <div className='z-20'>
-          <h2 className='text-[3em]'>Projects</h2>
-          <hr className='h-[2px] w-full mb-[2em]' />
-        </div>
+    <motion.section className={`relative `}>
+      <div className={` padding-x py-5 flex flex-col`}>
         <div
-          className='relative flex gap-10 w-full h-full md:overflow-hidden'>
-          <div
+          ref={itemsRef}
+          className='z-20 py-5'>
+          <h2 className='text-[3em]'>Projects</h2>
+          <hr className='h-[2px] w-full' />
+        </div>
+        <div className='relative flex gap-10 w-full h-full'>
+          <motion.div
+            style={{
+              display: isMobile ? selectWork === -1 ? "inline" : "none" : "inline",
+            }}
             ref={containerRef}
             onMouseMove={handleMouseMove}
             onMouseLeave={handleMouseLeave}
-            className={`relative ${(selectWork !== -1 && !isMobile) ? `flex-[.3]` : `flex-[1]`} w-full duration-700 transition-all ease-in-out`}>
+            className={`relative ${(selectWork !== -1 && !isMobile) ? `flex-[.3]` : `flex-[1]`} md:overflow-hidden w-full duration-700 transition-all ease-in-out`}>
             <motion.div
               style={{
                 y: moveY,
@@ -145,9 +149,8 @@ const Projects = () => {
                 duration: 1,
                 ease: "easeInOut"
               }}
-              ref={itemsRef}
               className='md:absolute md:translate-y-[-50%] md:top-[50%] md:w-[100%] w-[100%]  
-                         flex flex-col gap-16'>
+                         flex flex-col gap-10'>
               {MyWorks.map((item, i) => {
                 return (
                   <button key={i} onClick={() => setSelectWork(i)}
@@ -172,13 +175,17 @@ const Projects = () => {
               })
               }
             </motion.div>
-          </div>
-          <div className={`md:inline hidden flex-1`}>
+          </motion.div>
+          <motion.div
+            style={{
+              display: isMobile ? selectWork === -1 ? "none" : "inline" : "inline",
+            }}
+            className={`flex-1 relative`}>
             {selectWork === -1
-              ? <div className='grid place-items-center h-full'><h2 className='text-5xl'>Select one project</h2></div>
+              ? <div style={{height: `calc(100vh - ${heightTitle}px)`}} className={`grid place-items-center`}><h2 className='text-5xl italic'>Select a project</h2></div>
               : <Work project={MyWorks[selectWork]} setWork={setSelectWork} />
             }
-          </div>
+          </motion.div>
         </div>
       </div>
     </motion.section >
@@ -192,7 +199,7 @@ const Work = ({ project, setWork }: { project: ProjectItens, setWork: React.Disp
   return (
     <div
       key={project.title}
-      className='flex flex-col gap-2'>
+      className='flex flex-col gap-2 w-full'>
 
       <span className='cursor-pointer underline' onClick={() => setWork(-1)}>back to projects</span>
 
@@ -204,13 +211,14 @@ const Work = ({ project, setWork }: { project: ProjectItens, setWork: React.Disp
         alt="main"
         className='object-cover'
       />
-      <div className='flex gap-10 justify-between'>
-        <div className='w-[60%] flex flex-col gap-2'>
+      <div className='flex md:flex-row flex-col gap-10 md:justify-between'>
+
+        <div className='md:w-[60%] w-full flex flex-col gap-2'>
           <motion.h2
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.6, ease: "easeOut" }}
-            className='italic'
+            className='italic text-[1.2em]'
           >
             {project.title}
           </motion.h2>
@@ -218,28 +226,30 @@ const Work = ({ project, setWork }: { project: ProjectItens, setWork: React.Disp
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.6, ease: "easeOut" }}
-            className=''>
+            className='md:text-left text-justify'>
             {project.description}
           </motion.p>
         </div>
+
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
-          className=' flex flex-col  '>
-          <h2>Services</h2>
+          className=' flex flex-col ml-auto md:text-start text-end'>
+          <h2 className='underline'>Services</h2>
           {
             project.services.map((item) => (
               <p key={item}>{item}</p>
             ))
           }
         </motion.div>
+
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
-          className='flex flex-col gap-2'>
-          <h2>links</h2>
+          className='flex flex-col'>
+          <h2 className='underline'>links</h2>
           <Link href={project.linkGit} className='flex gap-2 items-center'>
             <GitHubLogoIcon />
             <p>Ntanzi/{project.title}</p>
